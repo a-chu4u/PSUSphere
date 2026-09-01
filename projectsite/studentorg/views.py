ddfbaf0981ced3fca622b581d7b4f1c20dfcd201
a-chu4u <aleahnana2006@gmail.com>
2026-09-01 22:44:41 +0800

    models again

--- projectsite/studentorg/views.py
+++ projectsite/studentorg/views.py
@@ -4,5 +4,11 @@ from studentorg.models import Organization
 
 class HomePageView(ListView):
     model = Organization
-    context_object_name = 'home'
+    context_object_name = "home"
     template_name = "home.html"
+
+class OrganizationList(ListView):
+    model = Organization
+    context_object_name = "organization"
+    template_name = "org_list.html"
+    paginate_by = 5
